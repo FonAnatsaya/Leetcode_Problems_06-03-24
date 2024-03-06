5dafdac632f48ae68ab2c19bf520c26849e3cb9d
fon <anatsaya.lee@gmail.com>
2024-03-06 13:25:45 +0700

    Finish Q1: Contain duplicate

--- Q1-Contains_Duplicate.js
+++ Q1-Contains_Duplicate.js
@@ -2,7 +2,7 @@
 
 // Example 1:
 // Input: nums = [1,2,3,1]
-// Output:
+// Output: true
 
 // Example 2:
 // Input: nums = [1,2,3,4]
@@ -11,3 +11,14 @@
 // Example 3:
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
+
+const containDuplicate = (arr) => {
+  arr.sort();
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) return true;
+  }
+  return false;
+};
+console.log(containDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log(containDuplicate([1, 2, 3, 4]));
+console.log(containDuplicate([1, 2, 3, 1]));
